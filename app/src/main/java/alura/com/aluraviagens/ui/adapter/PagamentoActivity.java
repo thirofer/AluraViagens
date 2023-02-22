d6d58c0af97c5669207f1c9b13f5e861540fa6a5
thirofer <thirofer@gmail.com>
2023-02-22 13:01:50 -0300

    Resumo compra

--- app/src/main/java/alura/com/aluraviagens/ui/adapter/PagamentoActivity.java
+++ app/src/main/java/alura/com/aluraviagens/ui/adapter/PagamentoActivity.java
@@ -1,15 +1,32 @@
 package alura.com.aluraviagens.ui.adapter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import java.math.BigDecimal;
+
+import alura.com.aluraviagens.MoedaUtil;
 import alura.com.aluraviagens.R;
+import alura.com.aluraviagens.ResumoPacoteActivity;
+import alura.com.aluraviagens.model.Pacote;
 
 public class PagamentoActivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR = "Pagamento de pacote";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagamento);
+
+        setTitle(TITULO_APPBAR);
+
+        Intent intent = new Intent (this, ResumoCompraActivity.class);
+        startActivity(intent);
+
     }
+
+
 }
---
+++ app/src/main/java/alura/com/aluraviagens/ui/adapter/ResumoCompraActivity.java
@@ -0,0 +1,14 @@
+package alura.com.aluraviagens.ui.adapter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import alura.com.aluraviagens.R;
+
+public class ResumoCompraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resumo_compra);
+    }
+}
